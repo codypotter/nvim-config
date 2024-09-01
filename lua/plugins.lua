f721a76ce7049066d5ab8f15656963f4d0dea505
Cody Potter <cody.potter@gearbox.com>
2024-08-31 20:12:02 -0400

    initial commit

---
+++ lua/plugins.lua
@@ -0,0 +1,48 @@
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use({
+    'rose-pine/neovim',
+    as = 'rose-pine',
+    config = function()
+      vim.cmd('colorscheme rose-pine')
+    end
+  })
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
+  use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
+  use('neovim/nvim-lspconfig')
+  use('hrsh7th/cmp-nvim-lsp')
+  use('hrsh7th/cmp-buffer')
+  use('williamboman/mason.nvim')
+  use('williamboman/mason-lspconfig.nvim')
+  use{ 'L3MON4D3/LuaSnip' }
+  use {
+    'hrsh7th/nvim-cmp',
+    config = function ()
+      require'cmp'.setup {
+      snippet = {
+        expand = function(args)
+          require'luasnip'.lsp_expand(args.body)
+        end
+      },
+
+      sources = {
+        { name = 'luasnip' },
+      },
+    }
+    end
+  }
+  use { 'saadparwaiz1/cmp_luasnip' }
+end)
